3eb7d05fcffad1d7e2b4d4f17f98e20a41a58e1a
影路灯 <1144827487@qq.com>
2025-07-28 22:15:10 +0800

    Update strings_07F5F0-07FC82.asm
    修改

--- strings/strings_07F5F0-07FC82.asm
+++ strings/strings_07F5F0-07FC82.asm
@@ -3,39 +3,39 @@
 
 String0x07F5F0:
 ; .strn "名探偵コナン　魔術師の挑戦状！{line}■製作スタッフ{line}{page}"
-  .strn "名侦探柯南　魔术师的挑战书！{line}　制作人员表{line}{page}"
+  .strn "名侦探柯南　魔术师的挑战书！　{line}■制作人员表　{line}{page}"
 
 String0x07F622:
 ; .strn "プロデューサー{line}　鈴木　敏弘{line}{page}"
-  .strn "制作人{line}　铃木　敏弘{line}{page}"
+  .strn "制作人　　　　{line}　铃木　敏弘{line}{page}"
 
 String0x07F642:
 ; .strn "アシスタント{line}　佐藤　俊{line}{page}"
-  .strn "制作助手{line}　佐藤　俊{line}{page}"
+  .strn "制作助手　　{line}　佐藤　俊{line}{page}"
 
 String0x07F65E:
 ; .strn "㈱キャラメル・ママ　スタッフ{line}{page}"
-  .strn "㈱牛奶糖　妈咪　制作人员{line}{page}"
+  .strn "㈱焦糖・妈妈　制作组　　　　{line}{page}"
 
 String0x07F67E:
 ; .strn "プロデューサー{line}　松本　常男{line}{page}"
-  .strn "制作人{line}　松本　常男{line}{page}"
+  .strn "制作人　　　　{line}　松本　常男{line}{page}"
 
 String0x07F69E:
 ; .strn "ディレクター{line}　古川　浩史{line}{page}"
-  .strn "导演{line}　古川　浩史{line}{page}"
+  .strn "导演　　　　{line}　古川　浩史{line}{page}"
 
 String0x07F6BC:
 ; .strn "企画サポート{line}　鈴木　永康　　仲西　覚{line}{page}"
-  .strn "企划担当{line}　铃木　永康　　仲西　觉{line}{page}"
+  .strn "企划担当　　{line}　铃木　永康　　仲西　觉{line}{page}"
 
 String0x07F6E6:
 ; .strn "シナリオ{line}　前田　美登里{line}{page}"
-  .strn "编剧{line}　前田　美登里{line}{page}"
+  .strn "编剧　　{line}　前田　美登里{line}{page}"
 
 String0x07F702:
 ; .strn "製作サポート{line}　笠井　秀樹{line}{page}"
-  .strn "制作担当{line}　笠井　秀树{line}{page}"
+  .strn "制作担当　　{line}　笠井　秀树{line}{page}"
 
 String0x07F720:
 ; .strn "　鈴木　身知子　村上　理子{line}　中原　紫恵{line}{page}"
@@ -47,59 +47,59 @@ String0x07F74C:
 
 String0x07F778:
 ; .strn "パブリシティー{line}　川熊　恵子{line}{page}"
-  .strn "广告宣传{line}　川熊　惠子{line}{page}"
+  .strn "广告宣传　　　{line}　川熊　惠子{line}{page}"
 
 String0x07F798:
 ; .strn "マニュアル{line}　岩原　誠　　　奥谷　博子{line}{page}"
-  .strn "说明书{line}　岩原　诚　　　奥谷　博子{line}{page}"
+  .strn "说明书　　{line}　岩原　诚　　　奥谷　博子{line}{page}"
 
 String0x07F7C2:
 ; .strn "㈱トムキャットシステム　スタッフ{line}{page}"
-  .strn "㈱汤姆猫程序　制作人员{line}{page}"
+  .strn "㈱汤姆猫程序　制作组　　　　　　{line}{page}"
 
 String0x07F7E6:
 ; .strn "ディレクター{line}　長島　正登{line}{page}"
-  .strn "导演{line}　长岛　正登{line}{page}"
+  .strn "导演　　　　{line}　长岛　正登{line}{page}"
 
 String0x07F804:
 ; .strn "プログラマー{line}　吉田　拓{line}{page}"
-  .strn "程序{line}　吉田　拓{line}{page}"
+  .strn "程序　　　　{line}　吉田　拓{line}{page}"
 
 String0x07F820:
 ; .strn "プランナー{line}　高橋　由紀夫{line}{page}"
-  .strn "统筹{line}　高桥　由纪夫{line}{page}"
+  .strn "统筹　　　{line}　高桥　由纪夫{line}{page}"
 
 String0x07F83E:
 ; .strn "デザイナー{line}　四家　直俊　　西野　和美{line}{page}"
-  .strn "设计师{line}　四家　直俊　　西野　和美{line}{page}"
+  .strn "设计师　　{line}　四家　直俊　　西野　和美{line}{page}"
 
 String0x07F868:
 ; .strn "デザイナー{line}　生地　憲{line}{page}"
-  .strn "设计师{line}　生地　宪{line}{page}"
+  .strn "设计师　　{line}　生地　宪{line}{page}"
 
 String0x07F882:
 ; .strn "デザイナー／轟画童{line}　高橋　久晴　　津島　大介{line}{page}"
-  .strn "设计师／轰画童{line}　高桥　久晴　　津岛　大介{line}{page}"
+  .strn "设计师／轰画童　　{line}　高桥　久晴　　津岛　大介{line}{page}"
 
 String0x07F8B4:
 ; .strn "デザイナー{line}　安野　隆志{line}{page}"
-  .strn "设计师{line}　安野　隆志{line}{page}"
+  .strn "设计师　　{line}　安野　隆志{line}{page}"
 
 String0x07F8D0:
 ; .strn "製作サポート{line}　吉井　敬子　　中川　大輔{line}{page}"
-  .strn "制作担当{line}　吉井　敬子　　中川　大辅{line}{page}"
+  .strn "制作担当　　{line}　吉井　敬子　　中川　大辅{line}{page}"
 
 String0x07F8FC:
 ; .strn "ゲストデザイナー{line}　宍倉　崇　　　古川　庸子{line}{page}"
-  .strn "客座设计师{line}　宍仓　崇　　　古川　庸子{line}{page}"
+  .strn "客位设计师　　　{line}　宍仓　崇　　　古川　庸子{line}{page}"
 
 String0x07F92C:
 ; .strn "サウンド／㈲ビートマニアック{line}　大久保　高嶺{line}{page}"
-  .strn "声效／㈲节奏狂热{line}　大久保　高岭{line}{page}"
+  .strn "声效／㈲节奏狂热　　　　　　{line}　大久保　高岭{line}{page}"
 
 String0x07F95C:
 ; .strn "スーパーバイザー{line}　大久保　良一{line}{page}"
-  .strn "检察员{line}　大久保　良一{line}{page}"
+  .strn "检察员　　　　　{line}　大久保　良一{line}{page}"
 
 String0x07F980:
 ; .strn "音楽{line}　山本　健司{line}{page}"
@@ -107,15 +107,15 @@ String0x07F980:
 
 String0x07F996:
 ; .strn "ＨＰ　Ｋ’Ｓ　ＷＯＲＬＤ{line}　臼田　恭子　　井村　文子{line}{page}"
-  .strn "ＨＰ　Ｋ　Ｓ　ＷＯＲＬＤ{line}　臼田　恭子　　井村　文子{line}{page}"
+  .strn "ＨＰ　Ｋ’Ｓ　ＷＯＲＬＤ{line}　臼田　恭子　　井村　文子{line}{page}"
 
 String0x07F9CE:
 ; .strn "テクニカルサポート{line}　花沢　確{line}{page}"
-  .strn "技术担当{line}　花泽　确{line}{page}"
+  .strn "技术担当　　　　　{line}　花泽　确{line}{page}"
 
 String0x07F9F0:
 ; .strn "少年サンデー編集部{line}　奥山　豊彦　　浅井　認{line}{page}"
-  .strn "周刊少年周日编辑部{line}　奥山　豊彦　　浅井　认{line}{page}"
+  .strn "周刊少年周日编辑部{line}　奥山　丰彦　　浅井　认{line}{page}"
 
 String0x07FA20:
 ; .strn "少年サンデー編集部{line}　石川　享　　　鈴木　翼{line}{page}"
@@ -123,11 +123,11 @@ String0x07FA20:
 
 String0x07FA50:
 ; .strn "小学館プロダクション{line}　三野　裕久　　杉山　裕子{line}{page}"
-  .strn "小学馆事务所{line}　三野　裕久　　杉山　裕子{line}{page}"
+  .strn "小学馆集英社　　　　{line}　三野　裕久　　杉山　裕子{line}{page}"
 
 String0x07FA84:
 ; .strn "東京ムービー{line}　吉岡　昌仁　　小林　弘明{line}{page}"
-  .strn "東京影像{line}　吉冈　昌仁　　小林　弘明{line}{page}"
+  .strn "東京影像　　{line}　吉冈　昌仁　　小林　弘明{line}{page}"
 
 String0x07FAB0:
 ; .strn "広報宣伝{line}　梅田　昌史　　島田　浩志{line}{page}"
@@ -135,15 +135,15 @@ String0x07FAB0:
 
 String0x07FAD8:
 ; .strn "広報宣伝{line}　佐藤　剛司　　笹野井　隆広{line}　愛甲　尚美{line}{page}"
-  .strn "广告宣传{line}　佐藤　冈司　　笹野井　隆广{line}　爱甲　尚美{line}{page}"
+  .strn "广告宣传{line}　佐藤　刚司　　笹野井　隆广{line}　爱甲　尚美{line}{page}"
 
 String0x07FB10:
 ; .strn "テストプレイ{line}　スタジオスタット{line}{page}"
-  .strn "试玩工作室{line}　制作人员{line}{page}"
+  .strn "试玩　　　　{line}　开始工作室　　　{line}{page}"
 
 String0x07FB34:
 ; .strn "スぺシャルサンクス{line}　田中　庸介　　佐々木　夕介{line}{page}"
-  .strn "特别鸣谢{line}　田中　庸介　　佐佐木　夕介{line}{page}"
+  .strn "特别鸣谢　　　　　{line}　田中　庸介　　佐佐木　夕介{line}{page}"
 
 String0x07FB68:
 ; .strn "　大矢知　洋一　門間　麻美{line}　金子　長武　　木下　和樹{line}{page}"
@@ -155,19 +155,19 @@ String0x07FBA2:
 
 String0x07FBC0:
 ; .strn "　唐鎌　寛樹　　山田　直樹{line}　小俁　淑子{line}{page}"
-  .strn "　唐鎌　寛樹　　山田　直樹{line}　小俣　淑子{line}{page}"
+  .strn "　唐镰　宽树　　山田　直树{line}　小俣　淑子{line}{page}"
 
 String0x07FBEC:
 ; .strn "■製作　　　　　　　ＢＡＮＤＡＩ{line}{page}"
-  .strn "　制作　　　　　　　ＢＡＮＤＡＩ{line}{page}"
+  .strn "■制作　　　　　　　ＢＡＮＤＡＩ{line}{page}"
 
 String0x07FC10:
 ; .strn "■著作　　（Ｃ）青山剛昌／小学館{line}　　読売テレビ・ＴＭＳ　１９９６{line}{page}"
-  .strn "　著作　　（Ｃ）青山刚昌／小学馆{line}　　读卖电视台　ＴＭＳ　１９９６{line}{page}"
+  .strn "■著作　　（Ｃ）青山刚昌／小学馆{line}　　读卖电视台　ＴＭＳ　１９９６{line}{page}"
 
 String0x07FC56:
 ; .strn "■著作{line}　　（Ｃ）ＢＡＮＤＡＩ　１９９９{line}{page}"
-  .strn "　著作{line}　　（Ｃ）ＢＡＮＤＡＩ　１９９９{line}{page}"
+  .strn "■著作{line}　　（Ｃ）ＢＡＮＤＡＩ　１９９９{line}{page}"
 
 .endarea
 
