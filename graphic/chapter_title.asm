5de2d1d8f62dc560181441da1e59a1c023f1939c
JohnTsq <msn451518580@hotmail.com>
2025-08-06 01:34:52 +0800

    ✨ feat(graphic): Add new chapter title graphics and tooling
    Added new Chinese chapter title graphics, tilemap, and tileset to gfx_0CBD7D. Updated chapter_title.asm to insert the new assets. Included encodegfx.bat for asset conversion and render_text.ipynb for generating text images.

--- graphic/chapter_title.asm
+++ graphic/chapter_title.asm
@@ -40,3 +40,9 @@
     "graphic\gfx_0BE67C\0F4999-0F61E0_chs.2bpp", \
     "graphic\gfx_0BE67C\recompressed_0F4999-0F61E0_chs.bin", \
     0x0F41A9,0x0F61E0
+
+.insertgfx \
+    "graphic\gfx_0CBD7D\0F61E0_chs.tilemap", \
+    "graphic\gfx_0CBD7D\0F69D0-0F82F9_chs.2bpp", \
+    "graphic\gfx_0CBD7D\recompressed_0F69D0-0F82F9_chs.bin", \
+    0x0F61E0,0x0F82F9
